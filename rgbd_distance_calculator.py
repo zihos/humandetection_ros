a3516e9e7701969e75a4c9644103893e16eee85b
zihos <jiho8345@naver.com>
2024-06-05 20:31:53 -0400

    update rgbd

--- rgbd_distance_calculator.py
+++ rgbd_distance_calculator.py
@@ -35,6 +35,7 @@ class RGBDProcessor:
         # Subscribers for color and depth images
         self.color_sub = Subscriber("/camera/color/image_raw", Image)
         self.depth_sub = Subscriber("/camera/depth/image_rect_raw", Image)
+        self.publisher = rospy.Publisher('detections', DetectionArray, queue_size=10)
 
         # ApproximateTimeSynchronizer to synchronize the topics
         self.ts = ApproximateTimeSynchronizer([self.color_sub, self.depth_sub], queue_size=10, slop=0.5)
@@ -42,6 +43,10 @@ class RGBDProcessor:
 
         self.color_image = None
         self.depth_image = None
+
+        self.red = (0, 0, 255)  # 빨간색
+        self.green = (0, 255, 0) # green
+        self.thickness = 1
 
         rospy.loginfo("RGBDProcessor Initialized")
 
@@ -98,73 +103,6 @@ class RGBDProcessor:
 
         return boxes_list
 
-    def parse_masks(self, results: Results) -> List[Mask]:
-
-        masks_list = []
-
-        def create_point2d(x: float, y: float) -> Point2D:
-            p = Point2D()
-            p.x = x
-            p.y = y
-            return p
-
-        mask: Masks
-        for mask in results.masks:
-
-            msg = Mask()
-
-            msg.data = [create_point2d(float(ele[0]), float(ele[1]))
-                        for ele in mask.xy[0].tolist()]
-            msg.height = results.orig_img.shape[0]
-            msg.width = results.orig_img.shape[1]
-
-            masks_list.append(msg)
-
-        return masks_list
-
-    def parse_keypoints(self, results: Results) -> List[KeyPoint2DArray]:
-
-        keypoints_list = []
-
-        points: Keypoints
-        for points in results.keypoints:
-
-            msg_array = KeyPoint2DArray()
-
-            if points.conf is None:
-                continue
-
-            for kp_id, (p, conf) in enumerate(zip(points.xy[0], points.conf[0])):
-
-                if conf >= self.threshold:
-                    msg = KeyPoint2D()
-
-                    msg.id = kp_id + 1
-                    msg.point.x = float(p[0])
-                    msg.point.y = float(p[1])
-                    msg.score = float(conf)
-
-                    msg_array.data.append(msg)
-
-            keypoints_list.append(msg_array)
-
-        return keypoints_list
-    
-    def get_min_distance_in_roi(self, depth_image: np.array, xmin: int, ymin: int, xmax: int, ymax: int) -> float:
-        roi = depth_image[ymin:ymax, xmin:xmax]
-        min_distance = np.min(roi)
-        return min_distance
-    
-    def get_distance_at_point(self, x, y):
-        if self.depth_image is not None:
-            distance = self.depth_image[y, x] / 1000.0
-            # rospy.loginfo(f"Distance at ({x}, {y}): {distance:.2f} meters")
-            return distance
-        else:
-            rospy.logwarn("Depth image not available")
-            return None
-
-
     def show_images(self):
         if self.color_image is not None and self.depth_image is not None:
             try:
@@ -186,37 +124,31 @@ class RGBDProcessor:
 
                 results: Results = results[0].cpu()
 
-                if results.boxes:
-                    hypothesis = self.parse_hypothesis(results)
-                    boxes = self.parse_boxes(results)
+                distance_list = []
 
-                if results.masks:
-                    masks = self.parse_masks(results)
-
-                if results.keypoints:
-                    keypoints = self.parse_keypoints(results)
+                # create detection msgs
+                detections_msg = DetectionArray()
 
                 for box in results.boxes:
+                    aux_msg = Detection()
+
+                    aux_msg.class_id = int(box.cls)
+                    aux_msg.class_name = self.yolo.names[int(box.cls)]
+                    aux_msg.score = float(box.conf)
+                    
                     xyxy = box.xyxy[0]
                     xywh = box.xywh[0]
 
-                    start_point = (int(xyxy[0]), int(xyxy[1]))
-                    end_point = (int(xyxy[2]), int(xyxy[3]))
-                    color = (0, 0, 255)  # 빨간색
-                    thickness = 2
-                    cv2.rectangle(resized_color_image, start_point, end_point, color, thickness)
+                    msg = BoundingBox2D()
+                    msg.center.position.x = xywh[0]
+                    msg.center.position.y = xywh[1]
+                    msg.size.x = xywh[2]
+                    msg.size.y = xywh[3]
+                    aux_msg.bbox = msg
 
-                    center_distance = self.get_distance_at_point(int(xywh[0]), int(xywh[1]))
-
-                    # 빨간색 점 그리기 (중앙에)
-                    color = (0, 0, 255)  # 빨간색 (BGR 형식)
-                    thickness = -1  # 채워진 원
-                    radius = 5  # 점의 반지름
-
-                    cv2.circle(resized_color_image, (int(xywh[0]), int(xywh[1])), radius, color, thickness)
-
-                    xmin, ymin, xmax, ymax = int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])
                     # Ensure the coordinates are within the image dimensions
+                    xmin, ymin, xmax, ymax = int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])
+                    
                     height, width = self.depth_image.shape
                     
                     xmin = max(0, xmin)
@@ -224,31 +156,36 @@ class RGBDProcessor:
                     xmax = min(width, xmax)
                     ymax = min(height, ymax)
 
+                    start_point = (xmin, ymin)
+                    end_point = (xmax, ymax)
+                    cv2.rectangle(resized_color_image, start_point, end_point, self.red, self.thickness)
+
                     # Get minimum distance in the ROI
                     # Extract the region of interest from the depth image
                     roi_depth = self.depth_image[ymin:ymax, xmin:xmax]
 
                     # Find the minimum distance within the ROI
                     min_distance = np.min(roi_depth[roi_depth > 0]) / 1000.0
+                    distance_list.append(round(min_distance, 2))
+                    aux_msg.distance = min_distance
 
-                    # Compute the average distance, ignoring NaNs and invalid values
-                    valid_depths = roi_depth[np.isfinite(roi_depth)]
-                    if valid_depths.size > 0:
-                        average_distance = np.mean(valid_depths) / 1000.0
+                    detections_msg.detections.append(aux_msg)
+
                     # min_distance = self.get_min_distance_in_roi(self.depth_image, start_point[0], start_point[1], end_point[0], end_point[1])
-                    bbox_text = f"{average_distance:.2f}m, {center_distance:.2f}m, {min_distance:.2f}m"
-                    rospy.loginfo(bbox_text)
-                    cv2.putText(resized_color_image, bbox_text, (start_point[0], start_point[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                    bbox_text = f"{min_distance}m"
+                    cv2.putText(resized_color_image, bbox_text, (start_point[0], start_point[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.green, 1)
 
-                    
+                self.publisher.publish(detections_msg)
+
                 # Combine color and depth images side by side
-                # combined_image = np.vstack((resized_color_image, depth_display))
+                combined_image = np.vstack((resized_color_image, depth_display))
+                rospy.loginfo(distance_list)
 
-                # # Display the combined images
-                # cv2.imshow("RGB and Depth Image", combined_image)
+                # Display the combined images
+                cv2.imshow("RGB and Depth Image", combined_image)
 
-                # if cv2.waitKey(1) & 0xFF == ord('q'):
-                #     rospy.signal_shutdown("User requested shutdown")
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    rospy.signal_shutdown("User requested shutdown")
                 
 
             except Exception as e:
